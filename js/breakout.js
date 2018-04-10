f91e1acd29c15b27fed182da70863b71e152ceaf
DavidCarberry <37617050+DavidCarberry@users.noreply.github.com>
2018-04-10 17:41:00 +0100

    make bricks disappear

--- js/breakout.js
+++ js/breakout.js
@@ -18,6 +18,56 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false; 
 
+//setup some bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+//hold the bricks
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+//function draws the bricks
+function drawBricks() {
+ 	for(c=0; c<brickColumnCount; c++) {
+ 		for(r=0; r<brickRowCount; r++) {
+		if(bricks[c][r].status == 1){
+			var brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
+ 			var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+ 			bricks[c][r].x = brickX;
+ 			bricks[c][r].y = brickY;
+ 			ctx.beginPath();
+ 			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+		    ctx.closePath; 
+			}
+		}
+	}
+}
+
+//collision Dector
+function collisionDection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var b = bricks[c][r];
+			if(b.status == 1) {
+			if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+				dy = -dy;
+				b.status = 0; 
+			    }
+			}
+		}
+	}
+}
 
 //function draws ball on canvas 
  function drawBall() {
@@ -41,6 +91,12 @@ function drawPaddle() {
  	//clear canvas
  	ctx.clearRect(0, 0, canvas.width, canvas.height);
  	
+	//collision detection
+	collisionDection();
+	
+	//draw the bricks
+	drawBricks();
+	
  	//draw the ball 
  	drawBall();
  	
